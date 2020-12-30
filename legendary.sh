9354c347bff671deb84da6aa63b178587bae58db
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2020-12-30 13:40:22 +0900

    reauth で認証できるように

--- legendary.sh
+++ legendary.sh
@@ -5,7 +5,9 @@ set -ue -o pipefail
 SCRIPT_DIR="$(cd "$(dirname "$(readlink "${0}")")"; pwd)"
 
 LEGENDARY_DIR="/mnt/e/Fgame/legendary"
-LEGENDARY_PATH="${LEGENDARY_DIR}/legendary.exe"
+LEGENDARY_EXE_NAME="legendary.exe"
+LEGENDARY_PATH="${LEGENDARY_DIR}/${LEGENDARY_EXE_NAME}"
+WINDOWS_LEGENDARY_DIR="$(wslpath -m "${LEGENDARY_DIR}")"
 WINDOWS_LEGENDARY_PATH="$(wslpath -m "${LEGENDARY_PATH}")"
 ALIAS_TXT_PATH="${SCRIPT_DIR}/alias.txt"
 
@@ -20,17 +22,32 @@ while read line; do
     args="${args//${alias}/${appname}}"
 done < "${ALIAS_TXT_PATH}"
 
-result="$(cmd.exe /c "${WINDOWS_LEGENDARY_PATH}" ${args})"
+main(){
+    local result="$(cmd.exe /c "${WINDOWS_LEGENDARY_PATH}" ${args})"
 
-if echo "${args}" | grep -E "list-games|list-installed|list-files|list-saves"; then
-    while read line; do
-        appname="${line%%,*}"
-        alias="${line#*,}"
-        if [ "${appname:-}" = "" ] || [ "${alias:-}" = "" ]; then
-            break
-        fi
-        result="$(echo "${result}" | sed -e "s@${appname}@${alias}@g")"
-    done < "${ALIAS_TXT_PATH}"
-fi
-result="$(echo "${result}" | sed -e "s@App name: \(.*\) | Ver@App name: \\\\e[31m\1\\\\e[39m | Ver@g")"
+    if echo "${args}" | grep -E "list-games|list-installed|list-files|list-saves"; then
+        while read line; do
+            appname="${line%%,*}"
+            alias="${line#*,}"
+            if [ "${appname:-}" = "" ] || [ "${alias:-}" = "" ]; then
+                break
+            fi
+            result="$(echo "${result}" | sed -e "s@${appname}@${alias}@g")"
+        done < "${ALIAS_TXT_PATH}"
+    fi
+    echo "${result}" | sed -e "s@App name: \(.*\) | Ver@App name: \\\\e[31m\1\\\\e[39m | Ver@g"
+}
+
+reauth(){
+    cmd.exe /c "${WINDOWS_LEGENDARY_DIR}/auth.bat"
+}
+
+case "${1}" in
+reauth)
+    result="$(reauth)"
+    ;;
+*)
+    result="$(main "${args}")"
+esac
+
 echo -e "${result}"
