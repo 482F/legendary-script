7e37e58df516371364556248c694a11e684a645b
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2020-12-30 09:06:21 +0900

    appname にエイリアスが使用できるように
    alias.txt にの形式で記述する
    ```
    appname1,alias1
    appname2,alias2
    appname2,alias3
    ```

--- legendary.sh
+++ legendary.sh
@@ -2,10 +2,34 @@
 
 set -ue -o pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "$(readlink "${0}")")"; pwd)"
+
 LEGENDARY_PATH="/mnt/e/Fgame/legendary/legendary.exe"
 WINDOWS_LEGENDARY_PATH="$(wslpath -m "/mnt/e/Fgame/legendary/legendary.exe")"
+ALIAS_TXT_PATH="${SCRIPT_DIR}/alias.txt"
 
 
 cd "$(dirname ${LEGENDARY_PATH})"
 
-cmd.exe /c "${WINDOWS_LEGENDARY_PATH}" "${@}"
+args="${@}"
+
+while read line; do
+    appname="${line%%,*}"
+    alias="${line#*,}"
+    args="${args//${alias}/${appname}}"
+done < "${ALIAS_TXT_PATH}"
+
+result="$(cmd.exe /c "${WINDOWS_LEGENDARY_PATH}" ${args})"
+
+if echo "${args}" | grep -E "list-games|list-installed|list-files|list-saves"; then
+    while read line; do
+        appname="${line%%,*}"
+        alias="${line#*,}"
+        if [ "${appname:-}" = "" ] || [ "${alias:-}" = "" ]; then
+            break
+        fi
+        result="$(echo "${result}" | sed -e "s@${appname}@${alias}@g")"
+    done < "${ALIAS_TXT_PATH}"
+fi
+result="$(echo "${result}" | sed -e "s@App name: \(.*\) | Ver@App name: \\\\e[31m\1\\\\e[39m | Ver@g")"
+echo -e "${result}"
